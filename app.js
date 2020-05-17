a1299f33cfc6a49da3167f83e0398aa6d306adf2
Tyler McBride <tymcbrid@gmail.com>
2020-05-16 20:46:19 -0700

    Class updates and app.js update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -33,3 +34,159 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+
+const employees = [];
+
+const newEmployee = [
+    {
+        type: "list",
+        name: "addPrompt",
+        message: "Would you like to add another employee?",
+        choices: ["Yes", "No"]
+    }
+]
+const employeeType = [
+    {
+        type: "list",
+        name: "selectType",
+        message: "Please select select the role of the new employee",
+        choices: ["Manager", "Engineer", "Intern", "Nevermind"]
+    }
+]
+const newManager = [
+    {
+        type: "input",
+        name: "managerName",
+        message: "Please enter the manager's name",
+     },
+     {
+        type: "input",
+        name: "managerID",
+        message: "Please enter the manager's employee ID number"
+     },
+     {
+        type: "input",
+        name: "managerEmail",
+        message: "Please enter the manager's email address"
+     },
+     {
+        type: "input",
+        name: "managerPhone",
+        message: "Please enter the manager's office phone number"
+     }
+]
+const newEngineer = [
+    {
+        type: "input",
+        name: "engineerName",
+        message: "Please enter the new engineer's name",
+     },
+     {
+        type: "input",
+        name: "engineerID",
+        message: "Please enter the new engineer's employee ID number"
+     },
+     {
+        type: "input",
+        name: "engineerEmail",
+        message: "Please enter the new engineer's email"
+     },
+     {
+        type: "input",
+        name: "engineerGitHubID",
+        message: "Please enter the new engineer's GitHub username"
+     }
+]
+const newIntern = [
+    {
+        type: "input",
+        name: "internName",
+        message: "Please enter the new intern's name",
+     },
+     {
+        type: "input",
+        name: "internID",
+        message: "Please enter the new intern's employee ID number"
+     },
+     {
+        type: "input",
+        name: "internEmail",
+        message: "Please enter the new intern's email"
+     },
+     {
+        type: "input",
+        name: "interngithubID",
+        message: "Please enter the new intern's GitHub username"
+     },
+     {
+        type: "input",
+        name: "internSchool",
+        message: "Please enter the new intern's school"
+     }
+]
+
+function addManager() {
+    inquirer
+        .prompt(newManager).then(data => {
+        let manager = new Manager(data.managerName, data.managerID, this.email = data.managerEmail, data.managerPhone)
+        employees.push(manager);
+        addAnother();
+    })
+}
+
+function addEngineer() {
+    inquirer
+        .prompt(newEngineer).then(data => {
+        let engineer = new Engineer(data.engineerName, data.engineerID, this.email = data.engineerEmail, data.engineerPhone)
+        employees.push(engineer);
+        addAnother();
+    })
+}
+
+function addIntern() {
+    inquirer
+        .prompt(newIntern).then(data => {
+        let intern = new Intern(data.internName, data.internID, this.email = data.internEmail, data.internPhone)
+        employees.push(intern);
+        addAnother();
+    })
+}
+
+function addEmployee() {
+    inquirer
+        .prompt(employeeType).then(data => {
+       if(data.selectType == "Manager") {
+        addManager();
+       }
+       else if (data.selectType == "Engineer"){
+        addEngineer();
+       }
+       else if (data.selectType == "Intern"){
+        addIntern();
+      }   
+      else if (data.selectType == "Nevermind"){
+        // ????
+      }  
+    //    else {
+    //        throw err;
+    //    }
+    })
+}
+
+function addAnother() {
+    inquirer
+        .prompt(newEmployee).then(data => {
+       if(data.addPrompt == "Yes") {
+        addEmployee();
+       }
+       else{
+       console.log("no more new employee");
+       }
+    })
+}
+
+
+addManager();
+
+
